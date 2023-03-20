882dccaf813ca31007c67505df84ec5dbb652dc9
Alysson Barrera <alyssonbarrera@outlook.com>
2023-03-20 13:44:58 -0300

    ✨ add rbac

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -3,7 +3,8 @@ import { search } from './search'
 import { nearby } from './nearby'
 
 import { FastifyInstance } from 'fastify'
-import { verifyJWT } from '../../middlewares/verify-jwt'
+import { verifyJWT } from '@/http/middlewares/verify-jwt'
+import { verifyUserRole } from '@/http/middlewares/verify-user-role'
 
 export async function gymRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
@@ -11,5 +12,5 @@ export async function gymRoutes(app: FastifyInstance) {
   app.get('/gyms/search', search)
   app.get('/gyms/nearby', nearby)
 
-  app.post('/gyms', create)
+  app.post('/gyms', { onRequest: [verifyUserRole('ADMIN')] }, create)
 }
